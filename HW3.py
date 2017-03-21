afb3f00c8960a05744b155ce18dc3c1e1aef5315
akshatbjain <akshatbjain93@gmail.com>
2017-03-21 09:37:05 -0400

    Corrected Jointnames, startconfig and goalconfig

--- HW3.py
+++ HW3.py
@@ -21,7 +21,7 @@ def tuckarms(env,robot):
     with env:
         jointnames = ['l_shoulder_lift_joint','l_elbow_flex_joint','l_wrist_flex_joint','r_shoulder_lift_joint','r_elbow_flex_joint','r_wrist_flex_joint']
         robot.SetActiveDOFs([robot.GetJoint(name).GetDOFIndex() for name in jointnames])
-        robot.SetActiveDOFValues([1.29023451,-2.32099996,-0.69800004,1.27843491,-2.32100002,-0.69799996]);        
+        robot.SetActiveDOFValues([1.29023451,-2.32099996,-0.69800004,1.27843491,-2.32100002,-0.69799996]);
         robot.GetController().SetDesired(robot.GetDOFValues());
     waitrobot(robot)
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     env = Environment()
     env.SetViewer('qtcoin')
-    env.Reset()        
+    env.Reset()
     # load a scene from ProjectRoom environment XML file
     env.Load('hw3.env.xml')
     time.sleep(0.1)
@@ -37,31 +37,29 @@ if __name__ == "__main__":
     # 1) get the 1st robot that is inside the loaded scene
     # 2) assign it to the variable named 'robot'
     robot = env.GetRobots()[0]
-
     ### INITIALIZE YOUR PLUGIN HERE ###
 
     ### END INITIALIZING YOUR PLUGIN ###
-   
+
 
     # tuck in the PR2's arms for driving
     tuckarms(env,robot);
-  
+
     #set start config
-    jointnames =['l_shoulder_pan_joint','l_shoulder_lift_joint','l_elbow_flex_joint','l_wrist_flex_joint','l_forearm_roll_joint','l_wrist_flex_joint','l_wrist_roll_joint']
-    robot.SetActiveDOFs([robot.GetJoint(name).GetDOFIndex() for name in jointnames])      
-    startconfig = [-0.15,0.075,-1.008,0,0,-0.11,0]
+    jointnames =['l_shoulder_pan_joint','l_shoulder_lift_joint','l_upper_arm_roll_joint','l_elbow_flex_joint','l_forearm_roll_joint','l_wrist_flex_joint','l_wrist_roll_joint']
+    robot.SetActiveDOFs([robot.GetJoint(name).GetDOFIndex() for name in jointnames])
+    startconfig = [-0.15,0.075,-1.008,-0.11,0,-0.11,0]
     robot.SetActiveDOFValues(startconfig);
     robot.GetController().SetDesired(robot.GetDOFValues());
     waitrobot(robot)
 
     with env:
-        goalconfig = [0.449,-0.201,-0.151,0,0,-0.11,0]
+        goalconfig = [0.449,-0.201,-0.151,-0.11,0,-0.11,0]
 
         ### YOUR CODE HERE ###
         ###call your plugin to plan, draw, and execute a path from the current configuration of the left arm to the goalconfig
- 
+            
         ### END OF YOUR CODE ###
     waitrobot(robot)
 
     raw_input("Press enter to exit...")
-
